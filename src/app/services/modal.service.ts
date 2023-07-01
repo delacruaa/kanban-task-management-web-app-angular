14d342bef3a1445f5d55da4d266bfcdda7f54a4b
kabdiberekeangular <135981302+kabdiberekeangular@users.noreply.github.com>
2023-07-01 10:36:02 +0600

    Update modal.service.ts

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -5,75 +5,34 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class ModalService {
-  addTaskModalIsOpen =new BehaviorSubject<boolean>(false)
-  viewTaskModalIsOpen =new BehaviorSubject<boolean>(false)
-  deleteTaskModalIsOpen =new BehaviorSubject<boolean>(false)
-  editTaskModalIsOpen=new BehaviorSubject<boolean>(false)
-  createBoardModalIsOpen=new BehaviorSubject<boolean>(false)
-  editBoardModalIsOpen=new BehaviorSubject<boolean>(false)
-  deleteBoardModalIsOpen=new BehaviorSubject<boolean>(false)
-  addColumnModalIsOpen=new BehaviorSubject<boolean>(false)
-  sidebarModalIsOpen=new BehaviorSubject<boolean>(false)
-  
+  addTaskModal =new BehaviorSubject<boolean>(false)
+  viewTaskModal =new BehaviorSubject<boolean>(false)
+  deleteTaskModal =new BehaviorSubject<boolean>(false)
+  editTaskModal=new BehaviorSubject<boolean>(false)
+  createBoardModal=new BehaviorSubject<boolean>(false)
+  editBoardModal=new BehaviorSubject<boolean>(false)
+  deleteBoardModal=new BehaviorSubject<boolean>(false)
+  addColumnModal=new BehaviorSubject<boolean>(false)
+  sidebarModal=new BehaviorSubject<boolean>(false)
+  modalArr=['addTaskModal','viewTaskModal','deleteTaskModal','editTaskModal','createBoardModal','deleteBoardModal','addColumnModal','sidebarModal','editBoardModal']
 
   openModal(modalName:string) {
+    this.modalArr.forEach(item=> {
+      if(modalName===item) {
+        //@ts-ignore
+        this[modalName].next(true)
+      }
+    })
     
-    if(modalName==='addTaskModal') {
-      this.addTaskModalIsOpen.next(true)
-    }
-    if(modalName==='viewTaskModal') {
-      this.viewTaskModalIsOpen.next(true)
-    }
-    if(modalName === 'deleteTaskModal') {
-      this.deleteTaskModalIsOpen.next(true)
-    }
-    if(modalName === 'editTaskModal') {
-      this.editTaskModalIsOpen.next(true)
-    }
-    if(modalName === 'createBoardModal') {
-      this.createBoardModalIsOpen.next(true)
-    }
-    if(modalName === 'editBoardModal') {
-      this.editBoardModalIsOpen.next(true)
-    }
-    if(modalName === 'deleteBoardModal') {
-      this.deleteBoardModalIsOpen.next(true)
-    }
-    if(modalName === 'addColumnModal') {
-      this.addColumnModalIsOpen.next(true)
-    }
-    if(modalName === 'sidebarModal') {
-      this.sidebarModalIsOpen.next(true)
-    }
+    
   }
 
   closeModal(modalName:string) {
-    if(modalName==='addTaskModal') {
-      this.addTaskModalIsOpen.next(false)
-    }
-    if(modalName==='viewTaskModal') {
-      this.viewTaskModalIsOpen.next(false)
-    }
-    if(modalName === 'deleteTaskModal') {
-      this.deleteTaskModalIsOpen.next(false)
-    }
-    if(modalName === 'editTaskModal') {
-      this.editTaskModalIsOpen.next(false)
-    }
-    if(modalName === 'createBoardModal') {
-      this.createBoardModalIsOpen.next(false)
-    }
-    if(modalName === 'editBoardModal') {
-      this.editBoardModalIsOpen.next(false)
-    }
-    if(modalName === 'deleteBoardModal') {
-      this.deleteBoardModalIsOpen.next(false)
-    }
-    if(modalName === 'addColumnModal') {
-      this.addColumnModalIsOpen.next(false)
-    }
-    if(modalName === 'sidebarModal') {
-      this.sidebarModalIsOpen.next(false)
-    }
+    this.modalArr.forEach(item=> {
+      if(modalName===item) {
+        //@ts-ignore
+        this[modalName].next(false)
+      }
+    })
   }
 }
